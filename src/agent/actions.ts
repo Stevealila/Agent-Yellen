0d1047531a28587cc9084cdcf40f7ab710c3a8e6
Stevealila <stevealila25@gmail.com>
2024-12-20 19:08:28 +0300

    Remove generate node - it is limiting the workflow's potentials

--- src/agent/actions.ts
+++ src/agent/actions.ts
@@ -1,13 +1,9 @@
-import { pull } from "langchain/hub"
-import { ChatPromptTemplate } from "@langchain/core/prompts"
 import { ChatGroq } from "@langchain/groq"
 import { GraphState } from "./state"
 import { tools } from "./tools"
 
 
-
 export const shouldContinue = async (state: typeof GraphState.State) => {
-    console.log("---CHECKING WORKFLOW CONTINUATION---")
     const { messages } = state
     const lastMessage = messages[messages.length - 1]
 
@@ -18,7 +14,6 @@ export const shouldContinue = async (state: typeof GraphState.State) => {
     console.log("---ENDING WORKFLOW---")
     return "__end__"
 }
-
 
 
 export const agent = async (state: typeof GraphState.State): Promise<Partial<typeof GraphState.State>> => {
@@ -32,39 +27,6 @@ export const agent = async (state: typeof GraphState.State): Promise<Partial<typ
     }).bindTools(tools)
 
     const response = await model.invoke(messages)
-    return {
-        messages: [response],
-    }
-}
-
-
-
-export const generate = async (state: typeof GraphState.State): Promise<Partial<typeof GraphState.State>> => {
-    console.log("---GENERATING RESPONSE---")
-
-    const { messages } = state
-    const question = messages[0].content as string
-
-    const lastToolMessage = messages.slice().reverse().find((msg) => msg.getType() === "tool")
-    if (!lastToolMessage) {
-        throw new Error("No tool message found in the conversation history")
-    }
-
-    const docs = lastToolMessage.content as string
-    const prompt = await pull<ChatPromptTemplate>("rlm/rag-prompt")
-
-    const llm = new ChatGroq({
-        model: "gemma2-9b-it",
-        temperature: 0,
-        streaming: true,
-    })
-
-    const ragChain = prompt.pipe(llm)
-    const response = await ragChain.invoke({
-        context: docs,
-        question,
-    })
-
     return {
         messages: [response],
     }
--- src/agent/graph.ts
+++ src/agent/graph.ts
@@ -1,24 +1,16 @@
 import { START, END, StateGraph } from "@langchain/langgraph"
 import { GraphState } from "./state"
 import { toolNode } from "./tools"
-import { agent, generate, shouldContinue } from "./actions"
+import { agent, shouldContinue } from "./actions"
 
 
 const graphBuilder = new StateGraph(GraphState)
+    // nodes
     .addNode("agent", agent)
     .addNode("tools", toolNode)
-    .addNode("generate", generate)
-
-graphBuilder.addEdge(START, "agent")
-graphBuilder.addConditionalEdges(
-    "agent",
-    shouldContinue,
-    {
-        tools: "tools",
-        __end__: END
-    }
-)
-graphBuilder.addEdge("tools", "generate")
-graphBuilder.addEdge("generate", END)
+    // edges
+    .addEdge(START, "agent")
+    .addConditionalEdges("agent", shouldContinue)
+    .addEdge("tools", END)
 
 export const graph = graphBuilder.compile()
